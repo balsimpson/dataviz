30b88d27dd55fa854984dec8a8998834556e8625
balsimpson <thirdi@gmail.com>
2016-06-16 18:24:36 +0530

    updated

--- sketch.js
+++ sketch.js
@@ -5,11 +5,19 @@ function preload() {
 }
 
 function setup() {
+  createCanvas(400,600);
   var ipl = data.ipl;
   for (var i = 0; i < ipl.length; i++) {
     createElement('h1', ipl[i].Batsman);
-    createElement('h2', ipl[i].Highest);
+    var highest = map(ipl[i].Runs, 0, 1000, 5, 100);
     var total = ipl[i].Runs;
-    createElement('h2', total);
+    var speed = ipl[i].Strikerate;
+    var name  = ipl[i].Batsman;
+
+    var x = random(width);
+    var y = random(height);
+
+    ellipse(x,y,highest,highest);
+    text(total,x,y);
   }
 }
