a61ccec7636b7642cf21d99058e308b1fc70276d
balsimpson <thirdi@gmail.com>
2016-06-16 20:50:44 +0530

    update

--- sketch.js
+++ sketch.js
@@ -1,4 +1,9 @@
+var circles = [];
 var data;
+var x;
+var y;
+var radius;
+var ipl;
 
 function preload() {
   data = loadJSON("runs.json");
@@ -6,18 +11,25 @@ function preload() {
 
 function setup() {
   createCanvas(400,600);
-  var ipl = data.ipl;
+  background(120);
+  fill(255, 0, 175, 100);
+  //noStroke();
+  ipl = data.ipl;
+  //random position
   for (var i = 0; i < ipl.length; i++) {
-    createElement('h1', ipl[i].Batsman);
-    var highest = map(ipl[i].Runs, 0, 1000, 5, 100);
-    var total = ipl[i].Runs;
-    var speed = ipl[i].Strikerate;
-    var name  = ipl[i].Batsman;
-
-    var x = random(width);
-    var y = random(height);
-
-    ellipse(x,y,highest,highest);
-    text(total,x,y);
+    var names  = ipl[i].Batsman;
+    //map circle size to total runs
+    radius = map(ipl[i].Runs, 0, 1000, 5, 100);
+    //map speed to strikerate
+    var speed = map(ipl[i].Strikerate, 0, 300, 0.05, 0.1);
+    ellipse(random(width-20), random(height-20), radius, radius);
   }
+}
+
+function draw() {
+  //background(120);
+    //fill(120);
+
+    x = x + speed;
+    y = y + speed;
 }
